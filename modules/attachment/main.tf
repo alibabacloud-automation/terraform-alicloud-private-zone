4fa16aebccd3babea14dea9052b672a09c68b90a
曾逸尘 <yichen.zyc@alibaba-inc.com>
2019-02-28 19:38:06 +0800

    修正错误
    修正错误

--- modules/attachment/main.tf
+++ modules/attachment/main.tf
@@ -1,5 +1,5 @@
 resource "alicloud_pvtz_zone_attachment" "attachment" {
   zone_id = "${var.zone_id}"
   vpc_ids = ["${var.vpc_id_list}"]
-  count   = "${var.vpc_id_list_count}"
+  count   = "${var.need_attachment ? 1 : 0}"
 }
